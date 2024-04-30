546609815f4f25862226c28c346ab5eb50a35626
layunsk <alvarolayu@gmail.com>
2024-04-30 18:55:17 +0200

    test

--- src/test/java/testVendedor.java
+++ src/test/java/testVendedor.java
@@ -27,16 +27,12 @@ public class testVendedor {
 
     @Test
     void testRegistrarVendedor() {
-        // Crear un objeto de vendedor para la prueba
+       
         Vendedor vendedor = new Vendedor("Nombre", "Apellido", "123456789", "correo@ejemplo.com", "contraseña", "Dirección", "Tienda", "Horario", "Imagen");
 
-        // Mockear el comportamiento del repositorio para retornar el vendedor creado
         when(vendedorRepository.save(any(Vendedor.class))).thenReturn(vendedor);
 
-        // Ejecutar el método del controlador
         Vendedor vendedorRegistrado = vendedorController.registrarVendedor(vendedor);
-
-        // Verificar que el vendedor retornado sea el mismo que el vendedor creado
         assertEquals(vendedor, vendedorRegistrado);
     }
 
@@ -45,14 +41,14 @@ public class testVendedor {
      @Test
     public void testCrearVendedor() {
         
-        // Crear un nuevo vendedor
+        
         Vendedor vendedor = new Vendedor();
         vendedor.setnombre("Homer Simpson");
         vendedor.setcorreo("homer@gmail.com");
         vendedor.setcontraseña("1234");
         vendedor.setdireccion("Calle amarilla");
         vendedorRepository.save(vendedor);
-        // Verificar que se haya creado correctamente
+        
         assertNotNull(vendedor);
         assertEquals("Homer Simpson", vendedor.getnombre());
         assertEquals("juan.perez@mail.com", vendedor.getcorreo());
