19c12a26d76418e9d4b8521786da48198532982c
layunsk <alvarolayu@gmail.com>
2024-04-30 18:52:14 +0200

    test

--- src/test/java/testComprador.java
+++ src/test/java/testComprador.java
@@ -3,7 +3,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Map;
