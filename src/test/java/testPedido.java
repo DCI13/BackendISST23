2e3e27ea99df92dd3591b87d62c6aefe8622de7e
layunsk <alvarolayu@gmail.com>
2024-05-01 11:04:16 +0200

    test

--- src/test/java/testPedido.java
+++ src/test/java/testPedido.java
@@ -1,9 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
